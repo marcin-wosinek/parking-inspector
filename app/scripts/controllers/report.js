b80910bddefd750334bc3eaff8e4de926de0510d
Marcin Wosinek <marcin.wosinek@gmail.com>
2015-04-26 21:36:25 +0100

    start work on geograpy

--- app/scripts/controllers/report.js
+++ app/scripts/controllers/report.js
@@ -8,10 +8,14 @@
  * Controller of the parkingInspectorApp
  */
 angular.module('parkingInspectorApp')
-  .controller('ReportCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ReportCtrl', function ($scope, $mdDialog) {
+    $scope.getLocation = function() {
+      $mdDialog.show(
+        $mdDialog.alert()
+        .title('Geograpy data is not available')
+        .content('I can get your latitude & longitude. Please provide it manualy.')
+        .ariaLabel('Geography data alert')
+        .ok('Got it!')
+      );
+    };
   });
